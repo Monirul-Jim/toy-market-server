0ddec6276546b09a0c3b31c678b81640ddeaa51c
unknown <Monirul-Jim>
2023-05-20 10:56:38 +0600

    inviron setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors())
@@ -14,7 +15,7 @@ app.get('/', (req, res) => {
 
 
 
-const uri = "mongodb+srv://Toy-Shop:mIN9QBBr9I4Mdzhg@cluster0.dsd2lyy.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dsd2lyy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
